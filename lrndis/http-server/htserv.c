b0dc7f388e5f52e156b53944e8d451c460ce169c
fetisov <fsenok@gmail.com>
2015-02-09 04:16:23 +0300

    license changed to MIT fixed bug with http page writing size

--- lrndis/http-server/htserv.c
+++ lrndis/http-server/htserv.c
@@ -1,19 +1,25 @@
 /*
- * Copyright (C) 2015 by Sergey Fetisov <fsenok@gmail.com>
+ * The MIT License (MIT)
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ * Copyright (c) 2015 by Sergey Fetisov <fsenok@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /*
@@ -252,7 +258,7 @@ void htserv_io(void) // state machine
 			continue;
 
 		// have data to send?
-		if (con->writed > con->wsended)
+		if (con->state != CSP_CLOSING && con->writed > con->wsended)
 		{
 			int count;
 			err_t err;
@@ -268,7 +274,7 @@ void htserv_io(void) // state machine
 		}
 
 		// data was successfully sended?
-		if (con->wacksize >= con->wsended && con->wsended > 0)
+		if (con->state != CSP_CLOSING && con->wacksize >= con->wsended && con->wsended > 0)
 		{
 			con->writed = 0;
 			con->wsended = 0;
@@ -383,8 +389,8 @@ int htcon_write(int index, const char *data, int size)
 	if (con->state != CSP_ACTIVE) return 0;
 	res = con->resp.cont_len - con->htcon.writed;
 	if (size < res) res = size;
-	if (con->wsize - con->writed < size)
-		size = con->wsize - con->writed;
+	if (con->wsize - con->writed < res)
+		res = con->wsize - con->writed;
 	if (res <= 0) return 0;
 	memcpy(con->wbuff + con->writed, data, res);
 	con->writed += res;
--- project/app.c
+++ project/app.c
@@ -1,19 +1,25 @@
 /*
- * Copyright (C) 2015 by Sergey Fetisov <fsenok@gmail.com>
+ * The MIT License (MIT)
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ * Copyright (c) 2015 by Sergey Fetisov <fsenok@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /*
@@ -181,7 +187,7 @@ void init_periph(void)
 
 bool dns_query_proc(const char *name, ip_addr_t *addr)
 {
-	if (strcmp(name, "run.stm") == 0)
+	if (strcmp(name, "run.stm") == 0 || strcmp(name, "www.run.stm") == 0)
 	{
 		addr->addr = *(uint32_t *)ipaddr;
 		return true;
@@ -257,10 +263,7 @@ int main(void)
 	while (dnserv_init(PADDR(ipaddr), 53, dns_query_proc) != ERR_OK) ;
 
 	htserv_on_req = on_http_req;
-
 	while (htserv_init(80) != ERR_OK) ;
-
-	uint32_t last_tcp_tmr = mtime();
 
 	while (1)
 	{
